801b5d18c119ed931b96924b27c0a54c3e8e5642
Abdelrhman Khaled <bedobakr65@gmail.com>
2025-08-17 15:31:06 +0300

    Repl test 4

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,19 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // auto-flush for cout/cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  bool repeat = true;
-  while (repeat) {
-    std::cout << "$ ";
     std::string input;
-    std::getline(std::cin, input);
-    std::cout << input << ": command not found" << std::endl;
-  }
-  return 0;
-}
+    std::cout << "$ ";
 
+    while (std::getline(std::cin, input)) {
+        if (!input.empty()) {
+            std::cout << input << ": command not found" << std::endl;
+        }
+        std::cout << "$ ";
+    }
+
+    return 0;
+}
