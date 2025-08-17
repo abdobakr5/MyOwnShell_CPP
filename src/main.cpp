9381993f8c45dd4c6a783ba8f315b30f0d68ad40
Abdelrhman Khaled <bedobakr65@gmail.com>
2025-08-17 16:33:21 +0300

    Add type logic and restructured the code.

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,48 @@ int main() {
 
     while (std::getline(std::cin, input)) {
         if (!input.empty()) {
+            
+            // exit
             if (input == "exit 0") {
                 return 0; 
             }
 
-            if (input.find("echo") == 0)
-            {
-              std::cout << input.substr(5,input.length()) << std::endl;
-              std::cout << "$ ";
-              continue;
+            // echo
+            if (input.rfind("echo", 0) == 0) {
+                if (input.size() > 5) {
+                    std::cout << input.substr(5) << std::endl;
+                } else {
+                    std::cout << std::endl;
+                }
+                std::cout << "$ ";
+                continue;
             }
-            
+
+            // type
+            if (input.rfind("type", 0) == 0) {
+                if (input.size() > 5) {
+                    std::string arg = input.substr(5);
+
+                    if (arg == "echo") {
+                        std::cout << "echo is a shell builtin" << std::endl;
+                    } else if (arg == "type") {
+                        std::cout << "type is a shell builtin" << std::endl;
+                    } else if (arg == "exit") {
+                        std::cout << "exit is a shell builtin" << std::endl;
+                    } else {
+                        std::cout << arg << ": not found" << std::endl;
+                    }
+                } else {
+                    std::cout << "type: missing argument" << std::endl;
+                }
+                std::cout << "$ ";
+                continue;
+            }
+
+            // unknown
             std::cout << input << ": command not found" << std::endl;
         }
+
         std::cout << "$ ";
     }
 
